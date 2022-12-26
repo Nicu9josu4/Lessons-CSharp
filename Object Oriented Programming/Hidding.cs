11c7866c15fbc802fd70fa602de6534ebbd4e662
Josu Ion <i.josu@moldcell.md>
2022-12-26 11:20:58 +0200

    a0001

---
+++ Object Oriented Programming/Hidding.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming
+{
+    // Ascunderea metodelor si a proprietatilo din clasa de baza 
+    internal class Hidding
+    {
+        internal static void Main()
+        {
+            Employee employee= new Employee();
+            employee.Name = "Aliosha";
+            employee.ID = 1;
+            employee.Company = "Moldcell";
+
+            employee.Print();
+        }
+    }
+
+    class Person
+    {
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public void Print()
+        {
+            Console.Write($"ID={ID} Name={Name} ");
+        }
+
+    }
+    class Employee : Person
+    {
+        public new string? Name
+        {
+            get { return $"Mr./Ms. {base.Name}"; }
+            set { base.Name = value; }
+        }
+        public string? Company { get; set; }
+
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Company={Company}");
+        }
+    }
+}
--- Object Oriented Programming/Program.cs
+++ Object Oriented Programming/Program.cs
@@ -1,14 +1,14 @@
-﻿using Object_Oriented_Programming;
+﻿//using Object_Oriented_Programming;
 
-// generalizarea tipului in C#
-Data<int> FirstData = new Data<int>();
-var fData1 = FirstData.Data1 = 1;
-var fData2 = FirstData.Data2 = 2;
-Console.WriteLine($"Sum of FirstData is:{fData2 + fData1}");
-Console.ReadKey();
+//// generalizarea tipului in C#
+//Data<int> FirstData = new Data<int>();
+//var fData1 = FirstData.Data1 = 1;
+//var fData2 = FirstData.Data2 = 2;
+//Console.WriteLine($"Sum of FirstData is:{fData2 + fData1}");
+//Console.ReadKey();
 
-Data<string> SecondData = new Data<string>();
-var sData1 = SecondData.Data1 = "1";
-var sData2 = SecondData.Data2 = "2";
-Console.WriteLine($"Sum of SecondData is:{sData2 + sData1}");
-Console.ReadKey();
+//Data<string> SecondData = new Data<string>();
+//var sData1 = SecondData.Data1 = "1";
+//var sData2 = SecondData.Data2 = "2";
+//Console.WriteLine($"Sum of SecondData is:{sData2 + sData1}");
+//Console.ReadKey();
